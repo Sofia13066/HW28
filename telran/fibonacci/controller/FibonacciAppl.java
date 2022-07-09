7032c045ae41ed956337e4485eaed2a98db848a9
Sofia13066 <podolskaya.sofya190498@gmail.com>
2022-07-09 23:28:35 +0300

    completed

--- telran/fibonacci/controller/FibonacciAppl.java
+++ telran/fibonacci/controller/FibonacciAppl.java
@@ -9,14 +9,20 @@ public class FibonacciAppl {
 	public static void main(String[] args) {
 		Fibonacci fibonacci = new Fibonacci(8);
         Iterator<Integer> iterator = fibonacci.iterator();
+        int sum = 0;
 		for (Integer num : fibonacci) {
             int curNum = 0;
+            
                 curNum = iterator.next();
+                sum += curNum;
                 System.out.print(curNum+",");
                 
                 
+                
 		}
+        System.out.println(" = " + sum);
 		// TODO print sum of all numbers 1,1,2,3,5,8,13,21 = 54
+
 
 	}
 
